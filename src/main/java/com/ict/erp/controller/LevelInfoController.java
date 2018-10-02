6bf0726504504e2f26c6f800b49393e721b5f6ab
pjw <llkwer@hanmail.net>
2018-10-02 13:23:27 +0900

    ict1 erp init

---
+++ src/main/java/com/ict/erp/controller/LevelInfoController.java
@@ -0,0 +1,10 @@
+package com.ict.erp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LevelInfoController {
+	
+	
+
+}
---
+++ src/main/java/com/ict/erp/dao/LevelInfoDAO.java
@@ -0,0 +1,5 @@
+package com.ict.erp.dao;
+
+public interface LevelInfoDAO {
+
+}
